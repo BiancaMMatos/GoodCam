7bc49ccc44301cd5a84a4eacb6c644fa226a88ce
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2025-11-30 19:33:00 -0300

    Adding the default project

---
+++ GoodCam/GoodCam/GoodCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodCamApp.swift
+//  GoodCam
+//
+//  Created by Bianca Maciel on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GoodCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoodCam/GoodCamTests/GoodCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoodCamTests.swift
+//  GoodCamTests
+//
+//  Created by Bianca Maciel on 30/11/25.
+//
+
+import Testing
+@testable import GoodCam
+
+struct GoodCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

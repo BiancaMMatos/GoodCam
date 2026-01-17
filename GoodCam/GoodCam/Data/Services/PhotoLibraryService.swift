892a8903c33a22405283dab458a936a9b605254e
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2026-01-16 21:12:39 -0300

    Adjusting the protocols in order to use the enum

--- GoodCam/GoodCam/Data/Services/PhotoLibraryService.swift
+++ GoodCam/GoodCam/Data/Services/PhotoLibraryService.swift
@@ -5,45 +5,58 @@
 //  Created by Bianca Maciel on 13/01/26.
 //
 
+import UIKit
 import Photos
 
 /// Using real photos
 final class PhotoLibraryService: PhotoLibraryServiceProtocol {
-    func fetchPhotos() async throws -> [PhotoAsset] {
+
+    func fetchPhotos(quality: PhotoImageQuality) async throws -> [PhotoAsset] {
+
         /// Ask for permission
         let status = await PHPhotoLibrary.requestAuthorization(for: .readWrite)
-        
-        guard status == .authorized || status == .limited else {
-            return []
-        }
-        
+        guard status == .authorized || status == .limited else { return [] }
+
         /// Search for assets
         let options = PHFetchOptions()
         options.sortDescriptors = [
             NSSortDescriptor(key: "creationDate", ascending: false)
         ]
-        
+
         let results = PHAsset.fetchAssets(with: .image, options: options)
-        
+
         /// Convert to UIImage
+        let scale = UIScreen().scale
+        
         return await withCheckedContinuation { continuation in
             var photos: [PhotoAsset] = []
-            
+
             let manager = PHImageManager.default()
             let requestOptions = PHImageRequestOptions()
+            requestOptions.deliveryMode = .highQualityFormat
+            requestOptions.resizeMode = .fast
             requestOptions.isSynchronous = true
-            requestOptions.deliveryMode = .fastFormat
-            
+
             results.enumerateObjects { asset, _, _ in
-                manager.requestImage(for: asset,
-                                     targetSize: .init(width: 300, height: 300),
-                                     contentMode: .aspectFill,
-                                     options: requestOptions) { image, _ in
+                manager.requestImage(
+                    for: asset,
+                    targetSize: quality == .thumbnail
+                        ? CGSize(width: 300 * scale, height: 300 * scale)
+                        : PHImageManagerMaximumSize,
+                    contentMode: .aspectFill,
+                    options: requestOptions
+                ) { image, _ in
                     if let image {
-                        photos.append(PhotoAsset(id: asset.localIdentifier, image: image))
+                        photos.append(
+                            PhotoAsset(
+                                id: asset.localIdentifier,
+                                image: image
+                            )
+                        )
                     }
                 }
             }
+
             continuation.resume(returning: photos)
         }
     }

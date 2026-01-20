ef8de1e35025789409791e2ee214b00433ebd7bc
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2026-01-20 19:14:14 -0300

    Adjusting Detail in order to send VM for horizontal grid

--- GoodCam/GoodCam/Presentation/Views/PhotoDetailView.swift
+++ GoodCam/GoodCam/Presentation/Views/PhotoDetailView.swift
@@ -8,24 +8,36 @@
 import SwiftUI
 
 struct PhotoDetailView: View {
-    
-    let photo: PhotoAsset
-    
+
+    @StateObject private var viewModel: PhotoDetailViewModel
+    @ObservedObject var libraryVM: PhotoLibraryViewModel
+
+    init(photo: PhotoAsset, libraryVM: PhotoLibraryViewModel) {
+        _viewModel = StateObject(wrappedValue: PhotoDetailViewModel(photo: photo))
+        self.libraryVM = libraryVM
+    }
+
     var body: some View {
         ZStack {
-            Color(.green)
-                .ignoresSafeArea()
             
-            Image(uiImage: photo.image)
-                .resizable()
-                .scaledToFit()
-                .padding()
-        }
-        .navigationTitle("Photo")
-        .navigationBarTitleDisplayMode(.inline)
-    }
-}
+            Color(.green).ignoresSafeArea()
+            
+            VStack {
+                Image(uiImage: viewModel.displayedImage)
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
 
-#Preview {
-    PhotoDetailView(photo: PhotoAsset.mockedPhotos[0])
+                PhotoLibraryHGrid(
+                    photos: libraryVM.photos,
+                    onSelect: { selected in
+                        viewModel.displayedImage = selected.image
+                    }
+                )
+                .frame(height: 120)
+            }
+            .navigationTitle("Photo")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
 }

062ea0b98591d3683d175cc6f18cccef61c3b2ba
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2026-01-20 22:29:08 -0300

    Adding logic to display image with filter

--- GoodCam/GoodCam/Presentation/ViewModels/PhotoDetailViewModel.swift
+++ GoodCam/GoodCam/Presentation/ViewModels/PhotoDetailViewModel.swift
@@ -5,18 +5,38 @@
 //  Created by Bianca Maciel on 20/01/26.
 //
 
-import SwiftUI
+import UIKit
 import Combine
 
 @MainActor
 final class PhotoDetailViewModel: ObservableObject {
 
-    let originalPhoto: PhotoAsset
-
+    @Published private(set) var originalImage: UIImage
     @Published var displayedImage: UIImage
 
-    init(photo: PhotoAsset) {
-        self.originalPhoto = photo
+    private let filtersService: FiltersService
+    let availableFilters: [CIFilter]
+
+    init(photo: PhotoAsset, filtersService: FiltersService) {
+        self.originalImage = photo.image
         self.displayedImage = photo.image
+        self.filtersService = filtersService
+        self.availableFilters = FiltersService.all()
+    }
+
+    func select(photo: PhotoAsset) {
+        originalImage = photo.image
+        displayedImage = photo.image
+    }
+
+    func apply(filter: CIFilter) {
+        displayedImage = filtersService.apply(
+            filter: filter,
+            to: originalImage
+        )
+    }
+
+    func reset() {
+        displayedImage = originalImage
     }
 }

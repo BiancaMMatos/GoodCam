3bd4c08d5256edf2cd2e9ac9cb15d9e6d9144c0e
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2026-01-20 23:04:36 -0300

    Adding logic of loading while waiting for photos from library

--- GoodCam/GoodCam/Presentation/Views/PhotoLibraryView.swift
+++ GoodCam/GoodCam/Presentation/Views/PhotoLibraryView.swift
@@ -11,18 +11,23 @@ struct PhotoLibraryView: View {
     let filtersService: FiltersService
     @State private var path = NavigationPath()
     @StateObject var viewModel: PhotoLibraryViewModel
-
+    
     var body: some View {
         NavigationStack(path: $path) {
             ZStack {
                 Color(.blue)
                     .ignoresSafeArea()
-
+                
                 ScrollView {
                     PhotoLibraryVGrid(photos: viewModel.photos, onSelect: { photo in
                         path.append(PhotoRouter.photoDetail(photo))
                     })
                     .padding(10)
+                }
+                
+                if viewModel.isLoading {
+                    ProgressView()
+                        .scaleEffect(1.4)
                 }
             }
             .navigationTitle("Library")
